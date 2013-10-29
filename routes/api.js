fc10ff087bb06d7063b857557082f363bd9ea5b0
Daniel Bidulock <dan.bidulock@hiregroundsoftware.com>
2013-10-29 15:59:55 -0600

    Remove outmoded response code

--- routes/api.js
+++ routes/api.js
@@ -1,15 +1,15 @@
 'use strict';
 
-var passport = require('passport'),
-    nconf = require('nconf');
+var passport = require('passport');//,
+//    nconf = require('nconf');
 
-nconf.argv().env().file({ file: 'local.json' });
-var gebo = require('../schemata/gebo')(nconf.get('email'));
+//nconf.argv().env().file({ file: 'local.json' });
+//var gebo = require('../schemata/gebo')(nconf.get('email'));
 
 exports.verify = [
     passport.authenticate('bearer', { session: false }),
     function (req, res) {
-        console.log('api');
+        console.log('api ---------------------------------');
         console.log(req.user);
         res.json(req.user);
       }
