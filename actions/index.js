4e97be18dfacc645d6c800ebcb28945ccc00660a
Daniel Bidulock <dan.bidulock@hiregroundsoftware.com>
2013-11-05 16:12:46 -0700

    Get actions index loader with tests

--- actions/index.js
+++ actions/index.js
@@ -1,13 +1,32 @@
+var utils = require('../lib/utils');
+
 /**
- * Greg Wang 2013-11-5
+ * Inspired by Greg Wang, 2013-11-5
  * http://stackoverflow.com/questions/5364928/node-js-require-all-files-in-a-folder
  *
  * Load `*.js` under current directory as properties
  * i.e., `User.js` will become `exports['User']` or `exports.User`
  */
-require('fs').readdirSync(__dirname + '/').forEach(function(file) {
-    if (file.match(/.+\.js/g) !== null && file !== 'index.js') {
-      var name = file.replace('.js', '');
-      exports[name] = require('./' + file);
-    }
-  });
+module.exports = function(email) {
+
+    // Turn the email into a mongo-friend database name
+    var dbName = utils.ensureDbName(email);
+
+    require('fs').readdirSync(__dirname + '/').forEach(function(file) {
+        if (file.match(/^\w+\.js/g) !== null && file !== 'index.js') {
+//          var name = file.replace('.js', '');
+          var actions = require('./' + file)(dbName);
+          var keys = Object.keys(actions);
+
+          for (var i = 0; i < keys.length; i++) {
+            if (exports[keys[i]]) {
+              throw new Exception('Two actions cannot have the same name');
+            }
+            exports[keys[i]] = actions[keys[i]];
+//            exports[name] = require('./' + file)(dbName);
+          }
+        }
+      });
+
+    return exports;
+  };
