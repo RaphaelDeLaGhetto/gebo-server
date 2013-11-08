9f6273e0d0dff0afd72cc83a7210b9d0cae1bd61
Daniel Bidulock <dan.bidulock@hiregroundsoftware.com>
2013-11-08 13:19:54 -0700

    Hard code site name into view

--- routes/basic.js
+++ routes/basic.js
@@ -1,11 +1,7 @@
 'use strict';
 
-var nconf = require('nconf');
-nconf.argv().env().file({ file: 'local.json' });
-
 exports.index = function (req, res) {
     res.render('index', {
-        title: nconf.get('name'),
         agent: req.user
       });
   };
