b7ff1dbc6b1a8ecbf978e55b730b84fa24ce0890
Daniel Bidulock <dan.bidulock@hiregroundsoftware.com>
2013-11-25 15:17:27 -0700

    Get sendMessageHandler

--- test/routes/message.js
+++ test/routes/message.js
@@ -1,8 +1,10 @@
 'use strict';
 
 var message = require('../../routes/message'),
+    nock = require('nock'),
     q = require('q'),
     extend = require('extend'),
+    nconf = require('nconf'),
     agentSchema = require('../../schemata/agent');
 
 var CLIENT = 'yanfen@example.com',
@@ -73,6 +75,11 @@ exports.sendMessageHandler = {
 
     'Should create a new conversation when no conversationId provided': function(test) {
         test.expect(13);
+
+        var scope = nock(nconf.get('domain')).
+                post('/receive').
+                reply(200, { data: 'Okay' });  
+
         var agentDb = new agentSchema(CLIENT);
         agentDb.conversationModel.find({}, function(err, conversations) {
             agentDb.connection.db.close();
@@ -87,6 +94,11 @@ exports.sendMessageHandler = {
             message.sendMessageHandler(SEND_REQ, RES, function(err, result) {
                     var agentDb = new agentSchema(CLIENT);
                     agentDb.conversationModel.find({}, function(err, conversations) {
+                            if (err) {
+                              console.log(err);
+                              test.ok(false, err);
+                            }
+                            scope.done();
                             agentDb.connection.db.close();
                             test.equal(conversations.length, 2);
                             test.equal(_code, 200);
@@ -106,6 +118,11 @@ exports.sendMessageHandler = {
 
     'Should return an ongoing conversation when a conversationId is provided': function(test) {
         test.expect(13);
+
+        var scope = nock(nconf.get('domain')).
+                post('/receive').
+                reply(200, { data: 'Okay' });  
+
         var agentDb = new agentSchema(CLIENT);
         agentDb.conversationModel.find({}, function(err, conversations) {
             agentDb.connection.db.close();
@@ -138,4 +155,87 @@ exports.sendMessageHandler = {
               });
           });
     },
+
+    'POST message to citizen agent if no gebo URI provided': function(test) {
+        test.expect(5);
+
+        var scope = nock(nconf.get('domain')).
+                post('/receive').
+                reply(200, { data: 'Okay' });  
+
+        message.sendMessageHandler(SEND_REQ, RES, function(err, result) {
+            scope.done();
+            test.equal(_code, 200);
+            test.equal(_content.conversationId.search('yanfen@example.com'), 0);
+            test.equal(_content.type, 'request');
+            test.equal(_content.socialCommitments.length, 1);
+            test.equal(_content.socialCommitments[0].performative, 'reply request');
+            test.done();
+          });
+     },
+
+    'POST message to foreign agent if gebo URI provided': function(test) {
+        test.expect(5);
+
+        var scope = nock('https://foreigngebo.com').
+                post('/receive').
+                reply(200, { data: 'Okay' });  
+
+        var req = { body: { gebo: 'https://foreigngebo.com' } };
+        extend(true, req, SEND_REQ);
+        message.sendMessageHandler(req, RES, function(err, result) {
+            test.equal(_code, 200);
+            test.equal(_content.conversationId.search('yanfen@example.com'), 0);
+            test.equal(_content.type, 'request');
+            test.equal(_content.socialCommitments.length, 1);
+            test.equal(_content.socialCommitments[0].performative, 'reply request');
+            test.done();
+          });
+    },
+};
+
+/**
+ * receive
+ */
+exports.receive = {
+    setUp: function(callback) {
+        _code = undefined;
+        _content = undefined;
+
+        var agentDb = new agentSchema(SERVER);
+        agentDb.connection.on('open', function(err) {
+            if (err) {
+              console.log(err)
+            }
+            agentDb.connection.db.dropDatabase(function(err) {
+                    var conversation = new agentDb.conversationModel({
+                            type: 'request',
+                            role: 'server',
+                            conversationId: 'Some conversation ID',
+                          });
+                    conversation.save(function(err) {
+                            agentDb.connection.db.close();
+                            if (err) {
+                              console.log(err);
+                            }
+                            callback();
+                          });
+              });
+          });
+    },
+
+    tearDown: function(callback) {
+        var agentDb = new agentSchema(SERVER);
+        agentDb.connection.on('open', function(err) {
+            agentDb.connection.db.dropDatabase(function(err) {
+                agentDb.connection.db.close();
+                if (err) {
+                  console.log(err)
+                }
+                callback();
+              });
+          });
+    },
+
+
 };
