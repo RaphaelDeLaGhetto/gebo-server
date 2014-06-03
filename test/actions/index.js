e48397f932c5c0705d47ac621d61b3798392faa0
Daniel Bidulock <dan.bidulock@hiregroundsoftware.com>
2014-06-03 09:28:43 -0600

    Mothball tests on mothballed functions

--- test/actions/index.js
+++ test/actions/index.js
@@ -9,18 +9,21 @@ var TEST_DB = utils.getMongoDbName(nconf.get('testDb'));
 exports.onLoad = {
 
     'Load and initialize every file in the actions folder': function(test) {
-        test.expect(15);
+        // 2014-6-3 Mothballed. Hold on for now...
+        //test.expect(15);
+        test.expect(12);
         // Note: actions, not action, as above
         var actions = require('../../actions')(TEST_DB);
-        test.equal(typeof actions.dbExists, 'function');
+        //test.equal(typeof actions.dbExists, 'function');
         test.equal(typeof actions.getCollection, 'function');
         test.equal(typeof actions.save, 'function');
         test.equal(typeof actions.cp, 'function');
         test.equal(typeof actions.rm, 'function');
         test.equal(typeof actions.rmdir, 'function');
         test.equal(typeof actions.ls, 'function');
-        test.equal(typeof actions.createDatabase, 'function');
-        test.equal(typeof actions.dropDatabase, 'function');
+        // 2014-6-3 Mothballed. Hold on for now...
+        //test.equal(typeof actions.createDatabase, 'function');
+        //test.equal(typeof actions.dropDatabase, 'function');
         test.equal(typeof actions.registerAgent, 'function');
         test.equal(typeof actions.deregisterAgent, 'function');
         test.equal(typeof actions.friend, 'function');
