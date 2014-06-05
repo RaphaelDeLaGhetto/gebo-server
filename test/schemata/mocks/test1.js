b9d86beb84027d077a7105acd7815a00c4a75c9a
Daniel Bidulock <dan.bidulock@hiregroundsoftware.com>
2014-06-05 14:03:16 -0600

    Update schemata mocks to reflect new single DB connection practices

--- test/schemata/mocks/test1.js
+++ test/schemata/mocks/test1.js
@@ -1,56 +1,32 @@
 
-var utils = require('../../../lib/utils');
+var geboDb = require('../../../lib/mongoose-connection'),
+    mongoose = require('mongoose'),
+    utils = require('../../../lib/utils');
 
 module.exports = function (email) {
 
-    // Turn the email into a mongo-friendly database name
-    var dbName = utils.ensureDbName(email);
-
-    var mongoose = require('mongoose');
-
-    /**
-     *  Database config
-     */
-    var uristring =
-        process.env.MONGOLAB_URI ||
-        process.env.MONGOHQ_URL ||
-        'mongodb://localhost/' + dbName;
-
-    var mongoOptions = { db: { safe: true } };
-
-    /**
-     * Connect to mongo
-     */
-    var connection = mongoose.createConnection(uristring, mongoOptions);
-
-    connection.on('open', function() {
-        console.log ('Successfully connected to: ' + uristring);
+    geboDb(function(connection) {
+        exports.connection = connection;
+    
+        //******* Database schema TODO add more validation
+        var Schema = mongoose.Schema,
+            ObjectId = Schema.Types.ObjectId;
+    
+        /**
+         * Test1 schema
+         */
+        var test1Schema = new Schema({
+            data: { type: String, required: true, unique: false },
+          });
+    
+        // Export test1Model
+        try {
+            var test1Model = connection.model('Test1', test1Schema);
+            exports.test1Model = test1Model;
+          }
+        catch (error) {}
+    
       });
-
-    connection.on('error', function(err) {
-        console.log ('ERROR connecting to: ' + uristring + '. ' + err);
-      });
-
-    exports.connection = connection;
-
-    //******* Database schema TODO add more validation
-    var Schema = mongoose.Schema,
-        ObjectId = Schema.Types.ObjectId;
-
-    /**
-     * Test1 schema
-     */
-    var test1Schema = new Schema({
-        data: { type: String, required: true, unique: false },
-      });
-
-    // Export test1Model
-    try {
-        var test1Model = connection.model('Test1', test1Schema);
-        exports.test1Model = test1Model;
-      }
-    catch (error) {}
 
     return exports;
   };
-
--- test/schemata/mocks/test2.js
+++ test/schemata/mocks/test2.js
@@ -1,74 +1,53 @@
 
-var utils = require('../../../lib/utils');
-
-module.exports = function (email) {
-
-    // Turn the email into a mongo-friendly database name
-    var dbName = utils.ensureDbName(email);
-
-    var mongoose = require('mongoose');
-
-    /**
-     *  Database config
-     */
-    var uristring =
-        process.env.MONGOLAB_URI ||
-        process.env.MONGOHQ_URL ||
-        'mongodb://localhost/' + dbName;
-
-    var mongoOptions = { db: { safe: true } };
-
-    /**
-     * Connect to mongo
-     */
-    var connection = mongoose.createConnection(uristring, mongoOptions);
-
-    connection.on('open', function() {
-        console.log ('Successfully connected to: ' + uristring);
-      });
-
-    connection.on('error', function(err) {
-        console.log ('ERROR connecting to: ' + uristring + '. ' + err);
-      });
-
-    /**
-     * This is handy for when I need to drop a database
-     * during testing
-     */
-    exports.connection = connection;
-
-    //******* Database schema TODO add more validation
-    var Schema = mongoose.Schema,
-        ObjectId = Schema.Types.ObjectId;
-
-    /**
-     * Test2 schema
-     */
-    var test2Schema = new Schema({
-        data: { type: String, required: true, unique: false },
-      });
-
-    // Export test2Model
-    try {
-        var test2Model = connection.model('Test2', test2Schema);
-        exports.test2Model = test2Model;
-      }
-    catch (error) {}
-
-    /**
-     * Test3 schema
-     */
-    var test3Schema = new Schema({
-        data: { type: String, required: true, unique: false },
-      });
-
-    // Export test3Model
-    try {
-        var test3Model = connection.model('Test2', test3Schema);
-        exports.test3Model = test3Model;
-      }
-    catch (error) {}
+var geboDb = require('../../../lib/mongoose-connection'),
+    mongoose = require('mongoose'),
+    utils = require('../../../lib/utils');
     
+module.exports = function() {
+
+    // Get the DB connection
+    geboDb(function(connection) {
+
+        /**
+         * This is handy for when I need to drop a database
+         * during testing
+         */
+        exports.connection = connection;
+    
+        //******* Database schema TODO add more validation
+        var Schema = mongoose.Schema,
+            ObjectId = Schema.Types.ObjectId;
+    
+        /**
+         * Test2 schema
+         */
+        var test2Schema = new Schema({
+            data: { type: String, required: true, unique: false },
+          });
+    
+        // Export test2Model
+        try {
+            var test2Model = connection.model('Test2', test2Schema);
+            exports.test2Model = test2Model;
+          }
+        catch (error) {}
+    
+        /**
+         * Test3 schema
+         */
+        var test3Schema = new Schema({
+            data: { type: String, required: true, unique: false },
+          });
+    
+        // Export test3Model
+        try {
+            var test3Model = connection.model('Test2', test3Schema);
+            exports.test3Model = test3Model;
+          }
+        catch (error) {}
+    
+    });
+
     return exports;
   };
 
