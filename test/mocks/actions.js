2614e1a115f03c35206312ff9c972883cd05b2a0
Daniel Bidulock <dan.bidulock@hiregroundsoftware.com>
2014-06-18 21:16:34 -0600

    Create action module skeletons

---
+++ test/mocks/actions.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = function() {
+
+    exports.someAction = function() {
+        
+      };
+
+    return exports;
+  };
---
+++ test/mocks/schema.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = function() {
+    exports.someModel = {};
+
+    return exports;
+  };
